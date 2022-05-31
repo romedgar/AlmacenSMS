5c1946b3ddf9b66ef9c60cb393aeb1cbf4277245
romedgar <62365991+romedgar@users.noreply.github.com>
2022-05-31 16:17:47 -0600

    Delete LADM_U4_Ejercicio3_RealtimeFirebase directory

--- LADM_U4_Ejercicio3_RealtimeFirebase/app/src/main/java/ittepic/edu/ladm_u4_ejercicio3_realtimefirebase/User.kt
+++
@@ -1,8 +0,0 @@
-package ittepic.edu.ladm_u4_ejercicio3_realtimefirebase
-
-
-import com.google.firebase.database.IgnoreExtraProperties
-
-@IgnoreExtraProperties
-data class User (val username : String?=null, val email: String ?=null) {
-}
